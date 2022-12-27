7e5287658efeeaf903489e9f9ad8752f77f7fa3e
Enrico Elizan <Enrico.Elizan@Siriusxm.com>
2022-12-27 10:51:26 -0500

    Added basic interface

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+import "./App.css";
+import "./normal.css";
+
+function App() {
+  return (
+    <div className="App">
+      <aside className="sidemenu">
+        <div className="side-menu-button">
+          <span>+</span>
+          New Chat
+        </div>
+      </aside>
+      <section className="chatbox"></section>
+    </div>
+  );
+}
+
+export default App;
